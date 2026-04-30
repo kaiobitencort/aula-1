b0de748232a79b175d0eaa0f35e5701cb7069e40
kaiobitencort <kaio.bitencort@escola.pr.gov.br>
2026-04-30 13:32:25 +0000

    atividade 1 finalizada

---
+++ array.js
@@ -0,0 +1,38 @@
+const edGames = [
+  {
+  titulo: "carnificina",
+  preco: 79
+  },
+  {
+    titulo: "spider man: homem aranha",
+    preco: 59
+  },
+  {
+    titulo: "edukof",
+    preco: 99
+  },
+  {
+    titulo: "felipe neto",
+    preco: 49
+  },
+  {
+    titulo: "it a coisa",
+    preco: 69
+  }
+]
+
+const edJames = [
+  {
+    titulo: "sons of de fhoret",
+    preco: 39
+  },
+  {
+    titulo: "ouro fino",
+    preco: 44
+  },
+  {
+    titulo: "huckmester",
+    preco: 34
+  },
+  ]
+    
---
+++ aula-2/index.js
@@ -0,0 +1,5 @@
+const listaLivros = require('./array');
+
+function mergeSort(array) {
+    const meio = Math.floor
+}
